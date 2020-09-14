39311c299cbb8251c70e44f98a550fd8804172f3
JasonFoxTrot <fox.jasonj@gmail.com>
2020-09-14 09:33:44 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLalchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLalchemy(app)
+
+class Todo(db.Model):
+  	id = db.Column(db.Interger, )
 
 @app.route('/')
 def index():
